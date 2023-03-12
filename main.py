2350bdb16d2e4b4a08c6e810842c224249f4102b
lukrzak <lukrzak@wp.pl>
2023-03-12 11:00:48 +0100

    Added gitignore and csv file read

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import csv
 file = open("WIG20.csv", "r")
+X_AXIS_COLUMN_NUMBER = 0
+Y_AXIS_COLUMN_NUMBER = 1
 x_axis = []
 y_axis = []
 
+data = csv.reader(file)
+for d in data:
+    x_axis.append(d[X_AXIS_COLUMN_NUMBER])
+    y_axis.append(float(d[Y_AXIS_COLUMN_NUMBER].replace('.', '').replace(',', '.')))
+
+print(x_axis)
+print(y_axis)
