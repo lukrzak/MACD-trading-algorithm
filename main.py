5987a9254cb8aa14db0b125ba0720e12b781cebd
lukrzak <lukrzak@wp.pl>
2023-03-12 11:59:03 +0100

    Added signal value calculation

--- main.py
+++ main.py
@@ -5,32 +5,36 @@ Y_AXIS_COLUMN_NUMBER = 1
 x_axis = []
 y_axis = []
 macd_values = []
+signal_values = []
 
-data = csv.reader(file)
-for d in data:
-    x_axis.append(d[X_AXIS_COLUMN_NUMBER])
-    y_axis.append(float(d[Y_AXIS_COLUMN_NUMBER].replace('.', '').replace(',', '.')))
+values = csv.reader(file)
+for i in values:
+    x_axis.append(i[X_AXIS_COLUMN_NUMBER])
+    y_axis.append(float(i[Y_AXIS_COLUMN_NUMBER].replace('.', '').replace(',', '.')))
 
 
-def get_ema_value(n, day):
+def get_ema_value(n, day, data):
     alpha = 2 / (n-1)
     ema_value = 0
-    ema_value_denom = 0
+    ema_value_denominator = 0
     for i in range(n):
         if day - i < 0:
             break
-        ema_value += ((1 - alpha) ** i) * y_axis[day - i]
-        ema_value_denom += ((1 - alpha) ** i)
-    return ema_value / ema_value_denom
+        ema_value += ((1 - alpha) ** i) * data[day - i]
+        ema_value_denominator += ((1 - alpha) ** i)
+    return ema_value / ema_value_denominator
 
 
 def calculate_macd_value():
     for i in range(len(y_axis)):
-        ema12 = get_ema_value(12, i)
-        ema26 = get_ema_value(26, i)
+        ema12 = get_ema_value(12, i, y_axis)
+        ema26 = get_ema_value(26, i, y_axis)
         macd = ema12 - ema26
         macd_values.append(macd)
+        signal = get_ema_value(9, i, macd_values)
+        signal_values.append(signal)
 
 
 calculate_macd_value()
 print(macd_values)
+print(signal_values)
